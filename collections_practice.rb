2cc0f77a80adfe2a79f1d9b9fe83987270cc0410
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-10 23:52:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,5 +42,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| element[-1]<<"s"}
+  array.each_with_index.collect{|element, index| element<<"s"}
 end
